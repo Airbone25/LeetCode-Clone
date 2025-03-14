23817e92c1ef9ecc69883e8bbdc52b07b342024f
Keshav <keshavmehra2005@gmail.com>
2025-03-14 21:24:15 +0530

    Basic Implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,79 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 
 function App() {
-  const [value, setValue] = useState("function addOne(a){\n\n}")
+  const [value, setValue] = useState("")
   const [result, setResult] = useState()
+  const [problems, setProblems] = useState()
+  const [problem, setProblem] = useState()
 
   const onChange = (value) => {
     setValue(value)
   }
 
-  async function postData(data){
+  useEffect(()=>{
+    getProblems()
+  }
+  ,[])
+
+  async function getProblems(){
+    const res = await fetch('http://localhost:3000/problems')
+    const data = await res.json()
+    console.log(data)
+    setProblems(data)
+  }
+
+  async function getProblem(id){
+    const res = await fetch(`http://localhost:3000/problems/${id}`)
+    const data = await res.json()
+    console.log(data)
+    setProblem(data)
+    setValue(`function outputFunction(${data.testCases[0].input.map((input,index)=>`input${index}`).join(',')}){\n\t\n}`)
+    setResult()
+  }
+
+  async function runCode(){
+    if(!problem){
+      return
+    }
     const res = await fetch('http://localhost:3000/code',{
       method: 'POST',
       headers:{
         'Content-Type':'application/json'
       },
-      body: JSON.stringify({data})
+      body: JSON.stringify({code:value,id: problems.find(p=>p.id == problem.id).id})
     })
     const results = await res.json()
     console.log(results)
     setResult(results)
   }
 
-  const onSubmit = ()=>{
-    postData(value)
-  }
-
   return (
     <div>
-      <CodeMirror
-        value={value}
-        height="200px"
-        extensions={[javascript()]}
-        onChange={onChange}
-      />
-      <button className="px-2 py-4 border-2 rounded-lg" onClick={onSubmit}>Submit</button>
       <div>
-        {result && result.map((res, index)=>(
-          <div key={index} className={`${res.toString() == 'true' ? 'text-green-500' : 'text-red-600'}`}>{res.toString()}</div>
-        ))}
+        <select onChange={(e)=>getProblem(e.target.value)}>
+          {problems && problems.map(problem=>(
+            <option key={problem.id} value={problem.id}>{problem.title}</option>
+          ))}
+        </select>
+      </div>
+
+      <div>
+        <h1>{problem && problem.title}</h1>
+        <p>{problem && problem.description}</p>
+        <CodeMirror
+          value={value}
+          height="200px"
+          extensions={[javascript()]}
+          onChange={onChange}
+        />
+        <button className="px-2 py-4 border-2 rounded-lg" onClick={runCode}>Submit</button>
+        <div>
+          {result && result.map((res, index)=>(
+            <div key={index} className={`${res.toString() == 'true' ? 'text-green-500' : 'text-red-600'}`}>{res.toString()}</div>
+          ))}
+        </div>
       </div>
     </div>
   )
--- server/index.js
+++ server/index.js
@@ -6,19 +6,49 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const testCases = [
-  {input: 1, output: 2},
-  {input: 2, output: 3},
-  {input: 3, output: 4}
+const problems = [
+  {
+    id: 1,
+    title: 'Add One',
+    description: 'Write a function that takes in a number and returns that number plus one.',
+    testCases: [
+      {input: [1], output: 2},
+      {input: [2], output: 3},
+      {input: [3], output: 4}
+    ]
+  },
+  {
+    id: 2,
+    title: 'Sum of Two',
+    description: 'Write a function that takes in two numbers and return the sum of the two numbers.',
+    testCases: [
+      {input: [1, 2], output: 3},
+      {input: [2, 3], output: 5},
+      {input: [3, 4], output: 7}
+    ]
+  }
 ]
 
+app.get('/problems',(req,res)=>{
+  res.json(problems)
+})
+
+app.get('/problems/:id',(req,res)=>{
+  const id = req.params.id
+  const problem = problems.find(problem=>problem.id == id)
+  res.json(problem)
+})
+
 app.post('/code', (req, res) => {
-    const code = req.body.data
+    const {code,id} = req.body
+    const problem = problems.find(p=>p.id == id)
+    
     const execute = `(function(){
       ${code}
       const results = []
+      const testCases = ${JSON.stringify(problem.testCases)}
       testCases.forEach(test=>{
-        const output = addOne(test.input)
+        const output = outputFunction(...test.input)
         results.push(output === test.output)
       })
       return results
