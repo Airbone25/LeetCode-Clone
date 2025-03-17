b47cf372bf7e652926529ad26c9af8d2c8fd90d9
Dhairya Summi <dhairya.summi@gmail.com>
2025-03-17 20:28:49 +0530

    done

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,29 +1,39 @@
-import React,{useEffect, useState} from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./HomePage.css";
 
 export default function HomePage() {
+  const [problems, setProblems] = useState();
 
-    const [problems,setProblems] = useState()
+  async function getProblems() {
+    const res = await fetch("https://leetcode-clone-api-umber.vercel.app/problems");
+    const data = await res.json();
+    setProblems(data);
+  }
 
-    async function getProblems(){
-        const res = await fetch('https://leetcode-clone-api-umber.vercel.app/problems')
-        const data = await res.json()
-        setProblems(data)
-    }
+  useEffect(() => {
+    getProblems();
+  }, []);
 
-    useEffect(()=>{
-        getProblems()
-    },[])
+return (
+    <div className="homepage">
+        <header className="homepage-header">
+            <nav className="homepage-nav">
+                <div className="logo">
+                    <img src="your-logo.png" alt="Logo" />
+                </div>
+                <div className="nav-links">
+                    <Link className="nav-link active" to="/home">Home</Link>
+                    <Link className="nav-link" to="/problems">Problems</Link>
+                    <Link className="nav-link" to="/contest">Contest</Link>
+                    <Link className="nav-link" to="/store">Store</Link>
+                </div>
 
-  return (
-    <div className='p-4 space-y-4 text-center'>
-        <h1 className='text-6xl'>LeetCode Clone</h1>
-        <h3 className='text-3xl'>Problems</h3>
-        <ul className='flex flex-col space-y-4'>
-            {problems && problems.map(problem=>(
-                <Link to={`/problems/${problem.id}`}><li key={problem.id} className='border-2 inline-block px-3'>{problem.title}</li></Link>
-            ))}
-        </ul>
+                <div className="right-icons">
+                    <Link className="nav-link" to="/signup">Sign Up</Link>
+                </div>
+            </nav>
+        </header>
     </div>
-  )
+);
 }
